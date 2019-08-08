62b02bb4703652773e6b7d8c3159505d4f0c8e7a
Jake Apple <jakeapple2010@gmail.com>
2019-08-08 14:30:44 -0600

    add end state

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,15 +15,9 @@ class Quiz extends Component {
     const isQuizEnd = quizPosition === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd ? (
-          <QuizEnd />
-        ) : (
+        {isQuizEnd ? <QuizEnd /> : 
           <QuizQuestion
-            quiz_question={
-              quizData.quiz_questions[this.state.quiz_position - 1]
-            }
-          />
-        )}
+            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
       </div>
     );
   }
